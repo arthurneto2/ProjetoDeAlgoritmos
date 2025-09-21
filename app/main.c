7ac518a4f1517e8ff280d4f6ecf92af7c66d270a
Arthur Neto <arthurneto779@gmail.com>
2025-09-21 16:40:56 -0300

    Inicia atualizaçao do menu para receber mais de um tipo de algoritmo

--- app/main.c
+++ app/main.c
@@ -22,79 +22,84 @@ int main() {
             return 0;
         }
 
-        if (algorithm_choice != 1) {
-            printf("Escolha invalida. Tente novamente.\n");
-            continue;
-        }
+        switch (algorithm_choice)
+        {
+        case 1:
+            printf("\n--- Tipo de Entrada ---\n");
+            printf("r. Randomico\n");
+            printf("c. Crescente\n");
+            printf("d. Decrescente\n");
+            printf("Escolha o tipo de entrada: ");
+            scanf(" %c", &data_type_choice);
 
-        printf("\n--- Tipo de Entrada ---\n");
-        printf("r. Randomico\n");
-        printf("c. Crescente\n");
-        printf("d. Decrescente\n");
-        printf("Escolha o tipo de entrada: ");
-        scanf(" %c", &data_type_choice);
+            printf("\n--- Tamanho da Entrada ---\n");
+            printf("1. 10\n");
+            printf("2. 100\n");
+            printf("3. 1.000\n");
+            printf("4. 10.000\n");
+            printf("5. 100.000\n");
+            printf("6. 1.000.000\n");
+            printf("Escolha o tamanho da entrada: ");
+            scanf("%d", &data_size_choice);
 
-        printf("\n--- Tamanho da Entrada ---\n");
-        printf("1. 10\n");
-        printf("2. 100\n");
-        printf("3. 1.000\n");
-        printf("4. 10.000\n");
-        printf("5. 100.000\n");
-        printf("6. 1.000.000\n");
-        printf("Escolha o tamanho da entrada: ");
-        scanf("%d", &data_size_choice);
-
-        SequenceType seq_type;
-        if (data_type_choice == 'c') {
-            seq_type = CRESCENTE;
-        } else if (data_type_choice == 'd') {
-            seq_type = DECRESCENTE;
-        } else if (data_type_choice == 'r') {
-            seq_type = RANDOM;
-        } else {
-            printf("Tipo de entrada invalido.\n");
-            continue;
-        }
-
-        int size;
-        switch (data_size_choice) {
-            case 1: size = 10; break;
-            case 2: size = 100; break;
-            case 3: size = 1000; break;
-            case 4: size = 10000; break;
-            case 5: size = 100000; break;
-            case 6: size = 1000000; break;
-            default:
-                printf("Tamanho de entrada invalido.\n");
+            SequenceType seq_type;
+            if (data_type_choice == 'c') {
+                seq_type = CRESCENTE;
+            } else if (data_type_choice == 'd') {
+                seq_type = DECRESCENTE;
+            } else if (data_type_choice == 'r') {
+                seq_type = RANDOM;
+            } else {
+                printf("Tipo de entrada invalido.\n");
                 continue;
+            }
+
+            int size;
+            switch (data_size_choice) {
+                case 1: size = 10; break;
+                case 2: size = 100; break;
+                case 3: size = 1000; break;
+                case 4: size = 10000; break;
+                case 5: size = 100000; break;
+                case 6: size = 1000000; break;
+                default:
+                    printf("Tamanho de entrada invalido.\n");
+                    continue;
+            }
+
+            printf("\nGerando sequencia de %d numeros...\n", size);
+            int* arr = generate_sequence(size, seq_type);
+            if (arr == NULL) {
+                fprintf(stderr, "Falha ao gerar a sequencia.\n");
+                continue;
+            }
+
+            printf("Salvando arquivo de entrada...\n");
+            write_input_file(seq_type, size, arr);
+
+            printf("Ordenando com Insertion Sort...\n");
+            clock_t start = clock();
+            insertion_sort(arr, size);
+            clock_t end = clock();
+
+            double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+            printf("Ordenacao concluida em %f segundos.\n", time_taken);
+
+            printf("Salvando arquivo de saida...\n");
+            write_output_file(seq_type, size, arr);
+
+            printf("Salvando arquivo de tempo...\n");
+            write_time_file(seq_type, size, time_taken);
+
+            free(arr);
+            printf("\nOperacao concluida com sucesso!\n");
+            
+            break;
+
+        default:
+            printf("Escolha invalida. Tente novamente.\n");
+            break;
         }
-
-        printf("\nGerando sequencia de %d numeros...\n", size);
-        int* arr = generate_sequence(size, seq_type);
-        if (arr == NULL) {
-            fprintf(stderr, "Falha ao gerar a sequencia.\n");
-            continue;
-        }
-
-        printf("Salvando arquivo de entrada...\n");
-        write_input_file(seq_type, size, arr);
-
-        printf("Ordenando com Insertion Sort...\n");
-        clock_t start = clock();
-        insertion_sort(arr, size);
-        clock_t end = clock();
-
-        double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
-        printf("Ordenacao concluida em %f segundos.\n", time_taken);
-
-        printf("Salvando arquivo de saida...\n");
-        write_output_file(seq_type, size, arr);
-
-        printf("Salvando arquivo de tempo...\n");
-        write_time_file(seq_type, size, time_taken);
-
-        free(arr);
-        printf("\nOperacao concluida com sucesso!\n");
     } while (algorithm_choice != 0);
 
     return 0;
